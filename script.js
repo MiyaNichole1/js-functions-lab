a4125e778d9bfe3491945b2e4ab1bbe9a97b5277
MiyaNichole1 <miyawalla123@gmail.com>
2024-06-29 03:09:05 -0500

    Final code for the rsp

--- script.js
+++ script.js
@@ -8,14 +8,14 @@
 // Exercise 1 has been completed for you:
 // */
 
-// const maxOfTwoNumbers = (x, y) => {
-//   if (x >= y) {
-//     return x;
-//   } else {
-//     return y;
-//   }
-// }
-// console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+const maxOfTwoNumbers = (x, y) => {
+  if (x >= y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
 
 
 
@@ -32,15 +32,15 @@
 // Complete the exercise in the space below:
 // */
 
-//  function isAdult() {
-//  age = 24;
-// if (age >= 18) {
-//   console.log('Adult');
-// } else {
-//   console.log('minor');
-// } 
-//  }
-//  console.log('Exercise 2 Result:', isAdult(24));
+ function isAdult() {
+ age = 24;
+if (age >= 18) {
+  console.log('Adult');
+} else {
+  console.log('minor');
+} 
+ }
+ console.log('Exercise 2 Result:', isAdult(24));
 
 
 
@@ -56,24 +56,24 @@ Example: isCharAVowel('a') should return true.
 
 Complete the exercise in the space below:
 */
-// function isCharAVowel() {
+function isCharAVowel() {
 
-// const character = 'y'.toLowerCase()
+const character = 'y'.toLowerCase()
 
-//  if (character === 'a' ) {
-//   console.log('true');
-//  } else if (character === 'e') {
-//   console.log('true');
-//  } else if (character === 'i') {
-//   console.log('true');
-//  } else if (character === 'o') {
-//   console.log('true');
-//  } else if (character === 'u') {
-//   console.log('true');
-//  } else {
-//   console.log('false');
-//  }
-// }
+ if (character === 'a' ) {
+  console.log('true');
+ } else if (character === 'e') {
+  console.log('true');
+ } else if (character === 'i') {
+  console.log('true');
+ } else if (character === 'o') {
+  console.log('true');
+ } else if (character === 'u') {
+  console.log('true');
+ } else {
+  console.log('false');
+ }
+}
 
 // console.log('Exercise 3 Result:', isCharAVowel("false"));
 
@@ -89,15 +89,15 @@ should return 'johnsmith@example.com'.
 
 Complete the exercise in the space below:
 */
-// function generateEmail() {
-//   let name = 'Kaylasmith';
-//   let domain = '@gmail.com';
-//   const email = `${name}${domain}`
-//   console.log(email);
-// } 
+function generateEmail() {
+  let name = 'Kaylasmith';
+  let domain = '@gmail.com';
+  const email = `${name}${domain}`
+  console.log(email);
+} 
 
 
-// console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
 
 /*
 Exercise 5: greetUser()
@@ -111,13 +111,13 @@ Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
 Complete the exercise in the space below:
 */
 
-// function greetUser() {
-//   let name = 'Bill';
-//   let timeOfDay = 'Good Morning';
-//   const greeting = `${timeOfDay}, ${name} ! :)`
-//   console.log(greeting);
-// }
-// console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+function greetUser() {
+  let name = 'Bill';
+  let timeOfDay = 'Good Morning';
+  const greeting = `${timeOfDay}, ${name} ! :)`
+  console.log(greeting);
+}
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
 
 
 /*
@@ -130,15 +130,15 @@ Example: maxOfThree(17, 4, 9) should return 17.
 
 Complete the exercise in the space below:
 */
-// function maxOfThree(){
-//    numA = 8;
-//    numB = 9;
-//    numC = 10;
+function maxOfThree(){
+   numA = 8;
+   numB = 9;
+   numC = 10;
 
-//    if (numC > numB && numA) {
-//      console.log(`The largest number is ${numC}`)
-//    }
-// }
+   if (numC > numB && numA) {
+     console.log(`The largest number is ${numC}`)
+   }
+}
 
 /*
 Exercise 7: calculateTip()
@@ -155,7 +155,9 @@ Complete the exercise in the space below:
 function calculateTip() {
   billAmount = 12;
   tip = .2;
-  tipAmount = ()
+  tipAmount = (billAmount * tip).toFixed(2)
+
+  console.log(`The tip amount is $ ${tipAmount}`)
 }
 
 
@@ -164,4 +166,72 @@ console.log('Exercise 7 Result:', calculateTip(50, 20));
 
 
 console.log('Exercise 6 Result:', maxOfThree(8, 9, 10));
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+ function convertTemperature() {
+
+Temp = 63
+
+celsiusConversion = ((Temp -32) * 5/9).toFixed() 
+
+fahrenheitConversion = ((Temp * 9/5 ) + 32).toFixed()
+
+console.log(`${Temp} 'F' is  ${celsiusConversion} Celsius`)
+console.log(`${Temp} 'C' is  ${fahrenheitConversion} Fahrenheit `)
+
+ }
+ console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+
+
+
+
+/* 
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+function basicCalculator () {
+
+    numA = 15
+  
+    numB =  9
+  
+    add = (numA + numB)
+    subtract = (numA - numB)
+    multiply = (numA * numB)
+    divide = (numA / numB).toFixed(2)
+  
+    console.log(divide)
+  
+  }
+  
+  console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+
+
+
+
+
 
